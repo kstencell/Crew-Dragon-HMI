7a391a61f3d3037c2f0d82d471165e0ef8bca812
Karl Stencell <karldstencell@gmail.com>
2021-11-09 13:59:04 -0500

    Added battery status GUI element that runs inside its own background worker thread. Also added a generator toggle button to recharge the battery that uses its own background worker thread.

---
+++ CrewDragonHMI/EnergyModule.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CrewDragonHMI
+{
+    public class EnergyModule
+    {
+
+        public static int batteryLevel;
+        public static string batteryFilePath = "BatteryLevel.txt";
+        public static bool generatorStatus;
+
+        public EnergyModule ()
+        {
+            batteryLevel = 50;
+            StreamReader batteryLevelStreamReader = new StreamReader(batteryFilePath);
+            batteryLevel = Int32.Parse(batteryLevelStreamReader.ReadLine());
+            batteryLevelStreamReader.Close();
+        }
+
+        public static int getBatteryLevel()
+        {
+            try
+            {
+                StreamReader batteryLevelStreamReader = new StreamReader(batteryFilePath);
+                batteryLevel = Int32.Parse(batteryLevelStreamReader.ReadLine());
+                batteryLevelStreamReader.Close();
+            } catch (IOException)
+            {
+                Thread.Sleep(50);
+            }
+
+            return batteryLevel;
+        }
+
+        public static void setBatteryLevel(int batteryLevel)
+        {
+            try
+            {
+                StreamWriter fileStream = new StreamWriter(batteryFilePath);
+                fileStream.WriteLine((batteryLevel).ToString());
+                fileStream.Close();
+            }
+            catch (IOException)
+            {
+                Thread.Sleep(50);
+            }
+        }
+
+
+    }
+}
--- CrewDragonHMI/MainWindow.xaml.cs
+++ CrewDragonHMI/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -13,6 +15,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace CrewDragonHMI
 {
     /// <summary>
@@ -20,19 +23,76 @@ namespace CrewDragonHMI
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        BackgroundWorker batteryIndicatorBackgroundWorker = new BackgroundWorker();
+        BackgroundWorker generatorIndicatorBackgroundWorker = new BackgroundWorker();
         public MainWindow()
         {
             InitializeComponent();
+            InitializeBatteryIndicatorBackgroundWorker();
+            InitializeGeneratorIndicatorBackgroundWorker();
         }
 
-        private void RichTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        private void InitializeBatteryIndicatorBackgroundWorker()
+        {
+            batteryIndicatorBackgroundWorker.WorkerReportsProgress = true;
+            batteryIndicatorBackgroundWorker.DoWork += BatteryIndicatorBackgroundWorker_DoWork;
+            batteryIndicatorBackgroundWorker.ProgressChanged += BatteryIndicatorBackgroundWorker_ProgressChanged;
+            batteryIndicatorBackgroundWorker.RunWorkerAsync();
+        }
+
+        private void BatteryIndicatorBackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (true)
+            {
+                int batteryLevel = EnergyModule.getBatteryLevel();
+                batteryIndicatorBackgroundWorker.ReportProgress(batteryLevel);
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+
+        private void BatteryIndicatorBackgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            BatteryIndicator.Value = e.ProgressPercentage;
+            BatteryIndicatorLabel.Text = "Battery Level: " + e.ProgressPercentage.ToString() + "%";
+        }
+
+        private void BatteryIndicator_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
 
         }
 
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        private void InitializeGeneratorIndicatorBackgroundWorker()
+        {
+            generatorIndicatorBackgroundWorker.WorkerReportsProgress = false;
+            generatorIndicatorBackgroundWorker.WorkerSupportsCancellation = true;
+            generatorIndicatorBackgroundWorker.DoWork += generatorIndicatorBackgroundWorker_DoWork;
+        }
+
+        private void generatorIndicatorBackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (!generatorIndicatorBackgroundWorker.CancellationPending)
+            {
+                System.Threading.Thread.Sleep(500);
+                EnergyModule.setBatteryLevel(EnergyModule.batteryLevel + 2);
+            }
+        }
+
+        private void GeneratorPower_Checked(object sender, RoutedEventArgs e)
         {
 
+            if (!generatorIndicatorBackgroundWorker.IsBusy)
+            {
+                generatorIndicatorBackgroundWorker.RunWorkerAsync();
+            }
+        }
+
+        private void GeneratorPower_Unchecked(object sender, RoutedEventArgs e)
+        {
+            if (generatorIndicatorBackgroundWorker.IsBusy)
+            {
+                generatorIndicatorBackgroundWorker.CancelAsync();
+            }
         }
     }
 }
