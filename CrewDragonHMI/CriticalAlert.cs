808de8605fe7e0b864eabf29457000b647f972f0
ehharvey <36806437+ehharvey@users.noreply.github.com>
2021-11-10 13:30:13 -0500

    Initial Design

---
+++ CrewDragonHMI/CriticalAlert.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrewDragonHMI
+{
+    class CriticalAlert
+    {
+        public bool isActive { get; private set; }
+        public bool onAlert { get; private set; }
+        public string alertMessage { get; private set; } // GOAL
+
+        public CriticalAlert(string file_name)
+        {
+
+        }
+
+
+    }
+}
