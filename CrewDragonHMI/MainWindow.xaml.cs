2e533f06a594ae0a8312b5412ffc9cefbf5e8a84
Torin Sparks <tsparks9038@conestogac.on.ca>
2021-11-12 23:53:38 -0500

    Completed fuel indicator.

--- CrewDragonHMI/MainWindow.xaml.cs
+++ CrewDragonHMI/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +22,38 @@ namespace CrewDragonHMI
     /// </summary>
     public partial class MainWindow : Window
     {
+        BackgroundWorker fuelIndicatorBackgroundWorker = new BackgroundWorker();
         public MainWindow()
         {
             InitializeComponent();
+            InitializeFuelIndicatorBackgroundWorker();
+        }
+
+        private void InitializeFuelIndicatorBackgroundWorker()
+        {
+            fuelIndicatorBackgroundWorker.WorkerReportsProgress = true;
+            fuelIndicatorBackgroundWorker.DoWork += FuelBackgroundWorker_DoWork;
+            fuelIndicatorBackgroundWorker.ProgressChanged += FuelBackgroundWorker_ProgressChanged;
+            fuelIndicatorBackgroundWorker.RunWorkerAsync();
+        }
+
+        private void FuelBackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (true)
+            {
+                int fuelLevel = MovementModule.getFuelLevel();
+                fuelIndicatorBackgroundWorker.ReportProgress(fuelLevel);
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+        private void FuelBackgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            FuelIndicator.Value = e.ProgressPercentage;
+        }
+
+        private void Fuel_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
         }
     }
 }
---
+++ CrewDragonHMI/MovementModule.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CrewDragonHMI
+{
+    public static class MovementModule
+    {
+        public static int fuelLevel;
+        public static string fuelFilePath = "FuelLevel.txt";
+
+        static MovementModule()
+        {
+            setFuelLevel(100);
+        }
+
+        public static int getFuelLevel()
+        {
+            try
+            {
+                StreamReader fuelLevelStreamReader = new StreamReader(fuelFilePath);
+                fuelLevel = Int32.Parse(fuelLevelStreamReader.ReadLine());
+                fuelLevelStreamReader.Close();
+            }
+
+            catch (IOException)
+            {
+                Thread.Sleep(50);
+            }
+
+            return fuelLevel;
+        }
+
+        public static void setFuelLevel(int fuelLevel)
+        {
+            try
+            {
+                StreamWriter fileStream = new StreamWriter(fuelFilePath);
+                fileStream.WriteLine((fuelLevel).ToString());
+                fileStream.Close();
+            }
+
+            catch (IOException)
+            {
+                Thread.Sleep(50);
+            }
+        }
+    }
+}
