ce1531556a2e6c86dff189f1acb8bf237e20c2c3
Karl Stencell <karldstencell@gmail.com>
2021-11-17 23:38:54 -0500

    added gui methods to integrate energy module and some of movement module into

--- CrewDragonHMI/MainWindow.xaml.cs
+++ CrewDragonHMI/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,149 @@ namespace CrewDragonHMI
     /// </summary>
     public partial class MainWindow : Window
     {
+        // *********************
+        // Energy Module Threads
+        // *********************
+        BackgroundWorker BW_battery = new BackgroundWorker();
+        BackgroundWorker BW_generator = new BackgroundWorker();
+        BackgroundWorker BW_shields = new BackgroundWorker();
+
+        // *********************
+        // Alert Module Threads
+        // *********************
+        BackgroundWorker BW_alert = new BackgroundWorker();
+
+        // ***********************
+        // Movement Module Threads
+        // ***********************
+        BackgroundWorker BW_fuel = new BackgroundWorker();
+        BackgroundWorker BW_speed = new BackgroundWorker();
+        BackgroundWorker BW_rotation = new BackgroundWorker();
+
+        // *********************************
+        // Exterior Integrity Module Threads
+        // *********************************
+        BackgroundWorker BW_hullIntegrity = new BackgroundWorker();
+
         public MainWindow()
         {
             InitializeComponent();
+            InitializeEnergyModule();
+            InitializeAlertModule();
+            InitializeExteriorIntegrityModule();
+            InitializeMovementModule();
+        }
+
+        /*******************************************/
+        /********** ENERGY MODULE METHODS **********/
+        /*******************************************/
+
+        private void InitializeEnergyModule()
+        {
+            BW_battery.WorkerReportsProgress = true;
+            BW_battery.DoWork += Battery_DoWork;
+            BW_battery.ProgressChanged += Battery_ProgressChanged;
+            BW_battery.RunWorkerAsync();
+
+            BW_generator.WorkerReportsProgress = false;
+            BW_generator.WorkerSupportsCancellation = true;
+            BW_generator.DoWork += Generator_DoWork;
+
+        }
+
+        // ***************
+        // *** BATTERY ***
+        // ***************
+
+        private void Battery_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (true)
+            {
+                int batteryLevel = EnergyModule.getBatteryLevel();
+                BW_battery.ReportProgress(batteryLevel);
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+
+        private void Battery_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            battery.Value = e.ProgressPercentage;
+            batteryText.Text = "Battery Level: " + e.ProgressPercentage.ToString() + "%";
+        }
+
+        // *****************
+        // *** GENERATOR ***
+        // *****************
+
+        private void Generator_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (!BW_generator.CancellationPending)
+            {
+                EnergyModule.setBatteryLevel(EnergyModule.batteryLevel + 1);
+                System.Threading.Thread.Sleep(250);
+            }
+        }
+
+        private void generator_Checked(object sender, RoutedEventArgs e)
+        {
+            if (!BW_generator.IsBusy)
+            {
+                BW_generator.RunWorkerAsync();
+            }
+        }
+
+        private void generator_Unchecked(object sender, RoutedEventArgs e)
+        {
+            if (BW_generator.IsBusy)
+            {
+                BW_generator.CancelAsync();
+            }
+        }
+
+        private void shields_Checked(object sender, RoutedEventArgs e)
+        {
+            EnergyModule.setShields();
+        }
+
+        private void shields_Unchecked(object sender, RoutedEventArgs e)
+        {
+            EnergyModule.setShields();
+        }
+
+        /*******************************************/
+        /********** ALERT MODULE METHODS ***********/
+        /*******************************************/
+
+        private void InitializeAlertModule()
+        {
+            // not sure if this needs a thread
+        }
+
+
+
+
+
+        /*******************************************/
+        /********* MOVEMENT MODULE METHODS *********/
+        /*******************************************/
+
+        private void InitializeMovementModule()
+        {
+            BW_fuel.WorkerReportsProgress = true;
+            BW_fuel.DoWork += Fuel_DoWork;
+            BW_fuel.ProgressChanged += Fuel_ProgressChanged;
+            BW_fuel.RunWorkerAsync();
+        }
+
+        // **************
+        // **** FUEL ****
+        // **************
+
+        private void speedChanger_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            MovementModule.speed = e.NewValue;
+            speedText.Text = e.NewValue + "km/s";
+
         }
     }
 }
