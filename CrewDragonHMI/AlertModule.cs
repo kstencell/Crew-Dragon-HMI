ebb23f65b8789c6594a027f65d74ce20e30f6293
ehharvey <36806437+ehharvey@users.noreply.github.com>
2021-11-22 12:58:01 -0500

    Implemented updating alert file functions. Migrated CriticalAlert -> AlertModule for consistency.

--- CrewDragonHMI/AlertModule.cs
+++ CrewDragonHMI/AlertModule.cs
@@ -1,12 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CrewDragonHMI
 {
-    class AlertModule
+    static class AlertModule
     {
+        static public bool isActive { get; private set; }
+        static public Dictionary<string, bool> onAlert { get; private set; }
+        static public Dictionary<string, int> alertThresholds { get; private set; }
+        static public string alertMessage { get; private set; } // GOAL
+
+        static public string alertFile { get; private set; }
+        static public string configFile { get; private set; }
+
+        static AlertModule()
+        {
+            alertFile = "alertStatus.txt";
+            configFile = "alertConfig.txt";
+        }
+
+        static private void UpdateAlertFile()
+        {
+            bool isOnAlert = false;
+            foreach(KeyValuePair<string, bool> entry in onAlert)
+            {
+                if (entry.Value)
+                {
+                    isOnAlert = true;
+                    break;
+                }
+            }
+
+            WriteAlert(isOnAlert);
+        }
+
+        static private void WriteAlert(bool isOnAlert)
+        {
+            using (StreamWriter sw = File.CreateText(alertFile))
+            {
+                if (isOnAlert)
+                {
+                    sw.WriteLine("yesalert");
+                }
+                else
+                {
+                    sw.WriteLine("noalert");
+                }
+            }
+        }
+
+        static private bool ReadAlert()
+        {
+            using (StreamReader sr = File.OpenText(alertFile))
+            {
+                string read_line = sr.ReadLine();
+
+                if (read_line == "yesalert")
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
     }
 }
