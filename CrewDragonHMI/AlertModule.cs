68488f015e957c7fb59ee19431ce1f233d4d61cf
ehharvey <36806437+ehharvey@users.noreply.github.com>
2021-11-22 17:09:55 -0500

    Completed initialization.

--- CrewDragonHMI/AlertModule.cs
+++ CrewDragonHMI/AlertModule.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text.Json;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics.CodeAnalysis;
+using System.Collections;
 
 namespace CrewDragonHMI
 {
+    class AlertConfig
+    {
+        public int BatteryThreshold { get; set; }
+        public int HullThreshold { get; set; }
+        public int FuelThreshold { get; set; }
+    }
+
+
     static class AlertModule
     {
         static public bool isActive { get; private set; }
@@ -14,19 +25,32 @@ namespace CrewDragonHMI
         static public Dictionary<string, int> alertThresholds { get; private set; }
         static public string alertMessage { get; private set; } // GOAL
 
+
         static public string alertFile { get; private set; }
         static public string configFile { get; private set; }
 
         static AlertModule()
         {
             alertFile = "alertStatus.txt";
-            configFile = "alertConfig.txt";
+            configFile = "alertConfig.json";
+
+            string configText = File.ReadAllText(configFile);
+
+            AlertConfig alertConfig = JsonSerializer.Deserialize<AlertConfig>(configText);
+
+            alertThresholds["BatteryThreshold"] = alertConfig.BatteryThreshold;
+            alertThresholds["HullThreshold"] = alertConfig.HullThreshold;
+            alertThresholds["FuelThreshold"] = alertConfig.FuelThreshold;
+
+            onAlert["BatteryThreshold"] = false;
+            onAlert["HullThreshold"] = false;
+            onAlert["FuelThreshold"] = false;
         }
 
         static private void UpdateAlertFile()
         {
             bool isOnAlert = false;
-            foreach(KeyValuePair<string, bool> entry in onAlert)
+            foreach (KeyValuePair<string, bool> entry in onAlert)
             {
                 if (entry.Value)
                 {
