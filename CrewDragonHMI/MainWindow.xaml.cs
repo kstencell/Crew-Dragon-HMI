901d1e8063ba4c505cd5c035e1a50ea53dbc516f
Karl Stencell <karldstencell@gmail.com>
2021-11-26 14:08:50 -0500

    Added a UI dial. Probably not the right branch but since the energy module was already merged, this is my playground.

--- CrewDragonHMI/MainWindow.xaml.cs
+++ CrewDragonHMI/MainWindow.xaml.cs
@@ -82,7 +82,7 @@ namespace CrewDragonHMI
         {
             while (true)
             {
-                int batteryLevel = EnergyModule.BatteryLevel;
+                int batteryLevel = EnergyModule.batteryLevel;
                 BW_battery.ReportProgress(batteryLevel);
                 System.Threading.Thread.Sleep(1000);
             }
@@ -102,7 +102,7 @@ namespace CrewDragonHMI
         {
             while (!BW_generator.CancellationPending)
             {
-                EnergyModule.BatteryLevel = EnergyModule.BatteryLevel + 1; // Having this variable completely public makes me queasy, but it is OK for now
+                EnergyModule.batteryLevel = EnergyModule.batteryLevel + 1; // Having this variable completely public makes me queasy, but it is OK for now
                 System.Threading.Thread.Sleep(250);
             }
         }
@@ -129,12 +129,12 @@ namespace CrewDragonHMI
 
         private void shields_Checked(object sender, RoutedEventArgs e)
         {
-            EnergyModule.SetShields(); // Double check this with requirements. Seems like a bool should be the parameter
+            //EnergyModule.SetShields(); // Double check this with requirements. Seems like a bool should be the parameter
         }
 
         private void shields_Unchecked(object sender, RoutedEventArgs e)
         {
-            EnergyModule.SetShields();
+            //EnergyModule.SetShields();
         }
 
 
@@ -239,6 +239,145 @@ namespace CrewDragonHMI
                 ExteriorIntegrityModule.HullIntegrity = newHullIntegrity;
                 System.Threading.Thread.Sleep(1000);
             }
+        }
+
+
+        // ******************
+        // ****** DIAL ******
+        // ******************
+
+
+        private bool _isPressed = false;
+        private Canvas _templateCanvas = null;
+
+        private void Ellipse_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            //Enable moving mouse to change the value.
+            _isPressed = true;
+        }
+
+        private void Ellipse_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            //Disable moving mouse to change the value.
+            _isPressed = false;
+        }
+
+        private void Ellipse_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_isPressed)
+            {
+                //Find the parent canvas.
+                if (_templateCanvas == null)
+                {
+                    _templateCanvas = MyHelper.FindParent<Canvas>(e.Source as Ellipse);
+                    if (_templateCanvas == null) return;
+                }
+                //Calculate the current rotation angle and set the value.
+                const double RADIUS = 150;
+                Point newPos = e.GetPosition(_templateCanvas);
+                double angle = MyHelper.GetAngleR(newPos, RADIUS);
+                knob.Value = (knob.Maximum - knob.Minimum) * angle / (2 * Math.PI);
+
+                this.Dispatcher.Invoke(() =>
+                {
+                    rotationText.Text = ((int)((angle * 180)/Math.PI)).ToString();
+                });
+            }
+        }
+    }
+
+    //The converter used to convert the value to the rotation angle.
+    public class ValueAngleConverter : IMultiValueConverter
+    {
+        #region IMultiValueConverter Members
+
+        public object Convert(object[] values, Type targetType, object parameter,
+                      System.Globalization.CultureInfo culture)
+        {
+            double value = (double)values[0];
+            double minimum = (double)values[1];
+            double maximum = (double)values[2];
+
+            return MyHelper.GetAngle(value, maximum, minimum);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter,
+              System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+
+    //Convert the value to text.
+    public class ValueTextConverter : IValueConverter
+    {
+
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter,
+                  System.Globalization.CultureInfo culture)
+        {
+            double v = (double)value;
+            return String.Format("{0:F2}", v);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+
+    public static class MyHelper
+    {
+        //Get the parent of an item.
+        public static T FindParent<T>(FrameworkElement current)
+          where T : FrameworkElement
+        {
+            do
+            {
+                current = VisualTreeHelper.GetParent(current) as FrameworkElement;
+                if (current is T)
+                {
+                    return (T)current;
+                }
+            }
+            while (current != null);
+            return null;
+        }
+
+        //Get the rotation angle from the value
+        public static double GetAngle(double value, double maximum, double minimum)
+        {
+            double current = (value / (maximum - minimum)) * 360;
+            if (current == 360)
+                current = 359.999;
+
+            return current;
+        }
+
+        //Get the rotation angle from the position of the mouse
+        public static double GetAngleR(Point pos, double radius)
+        {
+            //Calculate out the distance(r) between the center and the position
+            Point center = new Point(radius, radius);
+            double xDiff = center.X - pos.X;
+            double yDiff = center.Y - pos.Y;
+            double r = Math.Sqrt(xDiff * xDiff + yDiff * yDiff);
+
+            //Calculate the angle
+            double angle = Math.Acos((center.Y - pos.Y) / r);
+            Console.WriteLine("r:{0},y:{1},angle:{2}.", r, pos.Y, angle);
+            if (pos.X < radius)
+                angle = 2 * Math.PI - angle;
+            if (Double.IsNaN(angle))
+                return 0.0;
+            else
+                return angle;
         }
     }
 }
